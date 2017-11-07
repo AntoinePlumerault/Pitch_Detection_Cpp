4b64748b5427ddbcdd01e127d26598a9eb57a835
AntoinePlumerault <antoine.plumerault@gmail.com>
2017-11-07 11:49:42 +0100

    extraction of peaks in the spectrogram

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main()
 		printf("error");
 	}
 
-	unsigned int height = 800+400;
+	unsigned int height = 800+400+800;
 	unsigned int length = 800;
 	unsigned int margin = 20;
 
@@ -42,6 +42,20 @@ int main()
 	}
 	size_t buffer_pos = 0;
 
+	std::vector<std::vector<double>> list_fY; list_fY.resize(past);
+	for (size_t i = 0; i < past; ++i)
+	{
+		std::vector<double> fY; fY.resize(168);
+		list_fY[i] = fY;
+	}
+
+	std::vector<std::vector<double>> list_loc_max; list_loc_max.resize(past);
+	for (size_t i = 0; i < past; ++i)
+	{
+		std::vector<double> loc_max; loc_max.resize(168);
+		list_loc_max[i] = loc_max;
+	}
+
 	std::vector<double> X; X.resize(168);
 	for (size_t i = 0; i < 168; ++i)
 	{
@@ -50,6 +64,7 @@ int main()
 
 	while (window.isOpen())
 	{
+		auto tic = std::chrono::high_resolution_clock::now();
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -66,12 +81,44 @@ int main()
 
 		for (size_t i = 0; i < 168; i++) 
 		{
-			list_Y[buffer_pos][i] = log(1.0 + freqs[i]/10.0);
+			list_Y[buffer_pos][i] = freqs[i];
 		}
 		
+		for (size_t i = 1; i < 167; i++)
+		{
+			list_loc_max[buffer_pos][i] = 0.0;
+			if (list_Y[buffer_pos][i] > list_Y[buffer_pos][i+1] & list_Y[buffer_pos][i] > list_Y[buffer_pos][i - 1])
+			{
+				if (list_Y[buffer_pos][i] > 0.1)
+					list_loc_max[buffer_pos][i] = 0.1*(list_Y[buffer_pos][i] - (list_Y[buffer_pos][i + 1] + list_Y[buffer_pos][i - 1])/2.0)/ (0.05+list_Y[buffer_pos][i]);
+
+				if (list_Y[buffer_pos][i] < 0.5)
+					list_loc_max[buffer_pos][i] = 0.0;
+				else 
+					list_loc_max[buffer_pos][i] = 0.02;
+			}
+			
+		}
+		for (size_t i = 0; i < 168; ++i)
+		{
+			/*
+			list_fY[buffer_pos][i] = 0.0;
+			for (size_t k = 0; k < 4; ++k) 
+			{
+				if (round(i + 12.0 * log((double)k) / log(2.0)) < 168)
+				{
+					list_fY[buffer_pos][i] += 1.0/(1.0+k) * list_loc_max[buffer_pos][round(i + 12.0 * log((double)k) / log(2.0))];
+				}
+			}
+			list_fY[buffer_pos][i] = log(1.0 + list_fY[buffer_pos][i]/5.0)/2.0;
+			*/
+			//list_fY[buffer_pos][i] = 0.5*1.0/2.0 * (abs(list_Y[buffer_pos][i] - list_Y[(buffer_pos-1)%past][i]) + (list_Y[buffer_pos][i] - list_Y[(buffer_pos - 1) % past][i])) + 0.5*list_fY[(buffer_pos-1)%past][i];
+		}
+
 		for (size_t k = 1; k < past+1; ++k) 
 		{
-			std::vector<sf::Vertex> list_points = plot(list_Y[(buffer_pos + k) % past], X, "title", 168, 2.0, 2.1 * (1.0-((double)k / (double)past)));
+			double delta = 2.1 * (1.0 - ((double)k / (double)past));
+			std::vector<sf::Vertex> list_points = plot(list_Y[(buffer_pos + k) % past], X, "title", delta, 168, -delta, 2.0);
 
 			for (unsigned int line_i = 1; line_i < list_points.size(); ++line_i)
 			{
@@ -84,6 +131,24 @@ int main()
 					            255.0 * k / past, 
 					            255.0 * (1.0 - k / (3.0*past)), 
 					            255.0 * (past + k) / (2.0*past));
+				line[0].color = color;
+				line[1].color = color;
+				window.draw(line, 2, sf::Lines);
+			}
+
+			list_points = plot(list_loc_max[(buffer_pos + k) % past], X, "title", delta + 4.0, 168, -delta, 2.0);
+
+			for (unsigned int line_i = 1; line_i < list_points.size(); ++line_i)
+			{
+				sf::Vertex line[] =
+				{
+					list_points[line_i - 1],
+					list_points[line_i]
+				};
+				sf::Color color(255.0 * (1.0 - k / (3.0*past)),
+								255.0 * k / past,
+								255.0 * 0.0,
+								255.0 * (past + k) / (2.0*past));
 				line[0].color = color;
 				line[1].color = color;
 				window.draw(line, 2, sf::Lines);
@@ -108,6 +173,11 @@ int main()
 
 		window.display();
 		//===================================================//
+		auto toc = std::chrono::high_resolution_clock::now();
+		double elapsed_time = 1.e-6*std::chrono::duration_cast<std::chrono::nanoseconds>(toc - tic).count();
+		std::this_thread::sleep_for((std::chrono::milliseconds(size_t(1000.0/60.0 - elapsed_time))));
+		
+
 	}
 	recorder.stop();
 	return 0;
--- plot.hpp
+++ plot.hpp
@@ -2,12 +2,12 @@
 #define PLOT_H
 
 template<class T>
-std::vector<sf::Vertex> plot(const std::vector<T> Y, const std::vector<T> X, const std::string title, const float x_range, const float y_range, const float delta)
+std::vector<sf::Vertex> plot(const std::vector<T> Y, const std::vector<T> X, const std::string title, const float x_min,const float x_range, const float y_min, const float y_range)
 {
 	T max_X = *(std::max_element(std::begin(X), std::end(X)));
 	T min_X = *(std::min_element(std::begin(X), std::end(X)));
-	T max_Y = *(std::max_element(std::begin(Y), std::end(Y))) - delta;
-	T min_Y = *(std::min_element(std::begin(Y), std::end(Y))) - delta;
+	T max_Y = *(std::max_element(std::begin(Y), std::end(Y)));
+	T min_Y = *(std::min_element(std::begin(Y), std::end(Y)));
 	
 	T X_range = max_X - min_X;
 	T Y_range = max_Y - min_Y;
@@ -16,14 +16,18 @@ std::vector<sf::Vertex> plot(const std::vector<T> Y, const std::vector<T> X, con
 	unsigned int length = 800;
 	unsigned int margin = 20;
 
-	float X_scale = (float)(length-2.0*margin) / X_range;
+	float X_scale = (float)(length-2.0*margin) / x_range;
 	float Y_scale = (float)(height-2.0*margin) / y_range;
 
 	std::vector<sf::Vertex> list_points;
 	for (unsigned int point_i = 0; point_i < X.size(); ++point_i)
 	{
+		float x = (float)(X[point_i] - x_min)*X_scale + margin + x_min*200;
+		float y = (float)height - (float)(Y[point_i] - y_min)*Y_scale - margin;
+		/*
 		float x = (float)(X[point_i] - min_X)*X_scale + margin + delta*200;
 		float y = (float)height - (float)(Y[point_i] - min_Y)*Y_scale - margin;
+		*/
 		sf::Vertex point (sf::Vector2f(x, y));
 		list_points.push_back(point);
 	}
